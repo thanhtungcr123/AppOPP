4ed3a99b908f6f205d7b7906aeb3e1f3e98f500d
thanhtungcr123 <01694340878cr@gmail.com>
2019-10-02 14:23:53 +0700

    Add project files.

---
+++ AppOPP/StudentUniversity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppOPP
+{
+    class StudentUniversity:Student
+    {
+        public override void Talk()
+        {
+            base.Talk();
+        }
+    }
+}
